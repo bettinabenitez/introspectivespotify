e3c2dc89e1f5e6d3c43fe3dab81ccecb6e95e241
devikamehr <devikamehr@gmail.com>
2020-09-25 17:43:16 -0700

    Made an interactive bot that replies to commands.

--- P2A - Preliminary Architecture/bot.py
+++ P2A - Preliminary Architecture/bot.py
@@ -1,16 +1,30 @@
 # bot.py
 import os
-
-import discord
+import random
 from dotenv import load_dotenv
+
+
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
 
-@client.event
+bot = commands.Bot(command_prefix='!')
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
 
-client.run(TOKEN)
+@bot.command(name='Spotify', help='Pretends as if it were going to join your call, but it does not know how to right now.')
+async def spotify_(ctx):
+    spotify_commands = [
+        'Let me listen to some spotify!:musical_keyboard: ',
+        (
+            ':musical_note: Dropping in! :musical_note:'
+        ),
+    ]
+
+    response = random.choice(spotify_commands)
+    await ctx.send(response)
+
+bot.run(TOKEN)
