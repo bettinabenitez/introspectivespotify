65fa19128614403bf8fcbcade2f6ea3fbccd355d
William-La <wla@g.hmc.edu>
2020-11-19 01:17:20 -0800

    create function declarations for test file

---
+++ src/components/SpotifyAuth/auth_test.py
@@ -0,0 +1,32 @@
+import logging
+import unittest
+
+class AuthTest(unittest.TestCase):
+
+# BLACK BOX TESTS
+    def test_login(self):
+        pass
+
+    def test_logout(self):
+        pass
+    
+    def test_get_token(self):
+        pass
+
+    def test_refresh_token(self):
+        pass
+
+    def test_spotify_id(self):
+        pass
+
+
+# WHITE BOX TESTS
+    def test_login_permissions(self):
+        pass
+
+    def test_discord_username_change(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
