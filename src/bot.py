50cfb34bc8300d8e53265b8578e50b0daf45d9da
William-La <wla@g.hmc.edu>
2020-11-18 23:28:33 -0800

    add starter code for basic bot functionality

---
+++ src/bot.py
@@ -0,0 +1,14 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+bot = commands.Bot(command_prefix='!')
+
+bot.load_extension("components.InputClass")
+
+bot.run(DISCORD_TOKEN)
