534ff503befaaf2920a48018ee73d0d117bfbb49
William-La <wla@g.hmc.edu>
2020-11-18 23:40:02 -0800

    add skeleton for login and logout commands

--- src/components/InputClass/InputClass.py
+++ src/components/InputClass/InputClass.py
@@ -2,9 +2,21 @@ import os
 import discord
 from discord.ext import commands
 
+# import SpotifyAuth
+
 class InputClass(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def login(self, ctx):
+        user = ctx.author
+        # return SpotifyAuth.spotify_login(user)
+
+    @commands.command()
+    async def logout(self, ctx):
+        user = ctx.author
+        # return SpotifyAuth.spotify_login(user)
 
 def setup(bot):
     bot.add_cog(InputClass(bot))
