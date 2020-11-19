cdd568432326d03a834566dd35318e7e3fbfd5b2
William-La <wla@g.hmc.edu>
2020-11-18 23:37:02 -0800

    provide starter implementation for inputclass component

---
+++ src/components/InputClass/InputClass.py
@@ -0,0 +1,10 @@
+import os
+import discord
+from discord.ext import commands
+
+class InputClass(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+def setup(bot):
+    bot.add_cog(InputClass(bot))
