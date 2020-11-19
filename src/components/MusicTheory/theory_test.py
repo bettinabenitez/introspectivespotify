ab4da0d074486d959a70b24583aac30a010918fc
devikamehr <devikamehr@gmail.com>
2020-11-19 15:34:40 -0800

    Added __audio_features_help tests for MusicTheory.

--- src/components/MusicTheory/theory_test.py
+++ src/components/MusicTheory/theory_test.py
@@ -2,19 +2,37 @@ import unittest
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self)
-        self.assertEqual('foo'.upper(), 'FOO')
+    def audio_features_json(self)
+            hardCodedAF = {
+                            "danceability": 0.527,
+                            "energy": 0.134,
+                            "key": 4,
+                            "loudness": -11.264,
+                            "mode": 1,
+                            "speechiness": 0.0348,
+                            "acousticness": 0.932,
+                            "instrumentalness": 0,
+                            "liveness": 0.103,
+                            "valence": 0.479,
+                            "tempo": 98.879,
+                            "type": "audio_features",
+                            "id": "64yJ0tpcSveze3KJAdZGKe",
+                            "uri": "spotify:track:64yJ0tpcSveze3KJAdZGKe",
+                            "track_href": "https://api.spotify.com/v1/tracks/64yJ0tpcSveze3KJAdZGKe",
+                            "analysis_url": "https://api.spotify.com/v1/audio-analysis/64yJ0tpcSveze3KJAdZGKe",
+                            "duration_ms": 224107,
+                            "time_signature": 4
+                            }
+        self.assertEqual(MusicTheory.__audio_features_help("Thank You For The Music"), hardCodedAF)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def audio_features_error(self):
+        # Q! , so if there is nothing in here, it'll just have an 
+        # empty list. So how do I instead tell this inner private method 
+        # to just talk right back to the input class and say hey there is 
+        # nothing here and print this error instead of going back to my getter
+        # then back to the reply and back to Input !!! 
+        errorMessage = "Sorry! That track does not exist"
+        self.assertFalse(MusicTheory.__audio_features_help("Foo Bar Bash Bops"), errorMessage)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
