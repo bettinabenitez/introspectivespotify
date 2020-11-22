288d5f2774967ffb33c35a8f7988ae532ec61247
devikamehr <devikamehr@gmail.com>
2020-11-21 16:36:20 -0800

    Added all phase 1-3 commands for MT in InputClass.

--- src/components/InputClass/InputClass.py
+++ src/components/InputClass/InputClass.py
@@ -7,6 +7,16 @@ sys.path.append('../')
 from SpotifyAuth.SpotifyAuth import spotify_login
 from MusicHistory.MusicHistory import reply_top_songs_theory
 from MusicTheory.MusicTheory import reply_all_musictheory
+from MusicTheory.MusicTheory import reply_get_tempo 
+from MusicTheory.MusicTheory import reply_get_key 
+from MusicTheory.MusicTheory import reply_get_time_signature
+from MusicTheory.MusicTheory import reply_get_mode 
+from MusicTheory.MusicTheory import reply_get_mood 
+from MusicTheory.MusicTheory import reply_get_danceability 
+from MusicTheory.MusicTheory import reply_get_acousticness
+from MusicTheory.MusicTheory import reply_get_energy
+from MusicTheory.MusicTheory import reply_get_instrumentalness 
+
 
 # import MusicHistory
 
@@ -41,6 +51,51 @@ class InputClass(commands.Cog):
     @commands.command()
     async def musictheory(self, ctx, song):
         reply = reply_all_musictheory(song)
+        await ctx.send(reply)
+    
+    @commands.command()
+    async def tempo(self, ctx, song):
+        reply = reply_get_tempo(song)
+        await ctx.send(reply)
+    
+    @commands.command()
+    async def key(self, ctx, song):
+        reply = reply_get_key(song)
+        await ctx.send(reply)
+
+    @commands.command()
+    async def timesignature(self, ctx, song):
+        reply = reply_get_time_signature(song)
+        await ctx.send(reply)
+    
+    @commands.command()
+    async def mode(self, ctx, song):
+        reply = reply_get_mode(song)
+        await ctx.send(reply)
+
+    @commands.command()
+    async def mood(self, ctx, song):
+        reply = reply_get_mood(song)
+        await ctx.send(reply)
+    
+    @commands.command()
+    async def danceability(self, ctx, song):
+        reply = reply_get_danceability(song)
+        await ctx.send(reply)
+
+    @commands.command()
+    async def acousticness(self, ctx, song):
+        reply = reply_get_acousticness(song)
+        await ctx.send(reply)
+    
+    @commands.command()
+    async def energy(self, ctx, song):
+        reply = reply_get_energy(song)
+        await ctx.send(reply)
+
+    @commands.command()
+    async def instrumentalness(self, ctx, song):
+        reply = reply_get_instrumentalness(song)
         await ctx.send(reply)
         
 
