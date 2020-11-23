254867ed534ff405e636f75b51e59e4ef8475f08
emily <emilylu232@gmail.com>
2020-11-23 00:26:06 -0800

    wrote compute top song theory + helper + reply

--- src/components/MusicHistory/MusicHistory.py
+++ src/components/MusicHistory/MusicHistory.py
@@ -3,9 +3,18 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
+from statistics import multimode
 
 sys.path.append('../')
-import MusicTheory.MusicTheory 
+from MusicTheory.MusicTheory import get_tempo
+from MusicTheory.MusicTheory import get_key
+from MusicTheory.MusicTheory import get_instrumentalness
+from MusicTheory.MusicTheory import get_danceability
+from MusicTheory.MusicTheory import get_mode
+from MusicTheory.MusicTheory import get_time_signature
+from MusicTheory.MusicTheory import get_energy
+from MusicTheory.MusicTheory import get_acousticness
+from MusicTheory.MusicTheory import get_mood
 
 load_dotenv()
 
@@ -35,7 +44,10 @@ def compute_genre_helper(genre_dictionary, limit):
     :return: a queue of top genres sorted from most listened to genre to least listened to 
     """
     top_genre_queue = []
+
+    # sort the genre_dictionary based on how many artists each genre is associated with
     genre_dictionary = sorted(genre_dictionary, key=lambda k: len(genre_dictionary[k]), reverse=True)
+    
     count = 0
     for genre in genre_dictionary:
         if count < limit:
@@ -54,12 +66,12 @@ def compute_top_songs_theory_helper(theory_dictionary):
     :rtype: dictionary
     :return: a sorted dictionary with both the song IDs (key) and song names (value)
     """
-    # We will loop through the values (lists) in our output dictionary.
-    # For values of tempo, mode, danceability, acousticness, energy, and instrumentalness,
-    # we will find the average of the list and reset the list to a list containing only the average value.
-    # For values of key and time_signature, then we will reset the list as a list
-    # containing the element with the most occurrences (the mode). If there are ties, then the new list will include all the tied elements.
-    # Returns a dictionary
+    for feature in theory_dictionary:
+        if feature == "key" or feature == "mode":
+            theory_dictionary[feature] = multimode(theory_dictionary[feature])
+        else:
+            theory_dictionary[feature] = sum(theory_dictionary[feature]) / len(theory_dictionary[feature])
+    return theory_dictionary
 
 ###############################
 ##### COMPUTATION METHODS #####
@@ -180,20 +192,20 @@ def compute_top_songs_theory(top_songs):
                         "instrumentalness": []}
 
     for song in top_songs:
-        song = str(song[0]) + str(song[1])
-        theory_dictionary["tempo"].append(int(get_tempo(song)))
-        theory_dictionary["key"].append(int(get_key(song)))
-        theory_dictionary["mode"].append(int(get_mode(song)))
-        theory_dictionary["mood"].append(int(get_mood(song)))
-        theory_dictionary["danceability"].append(int(get_danceability(song)))
-        theory_dictionary["acousticness"].append(int(get_acousticness(song)))
-        theory_dictionary["energy"].append(int(get_energy(song)))
-        theory_dictionary["instrumentalness"].append(int(get_instrumentalness(song)))
-    print(theory_dictionary)
-    #return compute_top_songs_theory_helper(theory_dictionary)
-#print(compute_top_songs_theory())
+        song = (song[0]) + " " + (song[1]) # song name + artist name 
+        theory_dictionary["tempo"].append(float(get_tempo(song)))
+        theory_dictionary["time_signature"].append(int(get_time_signature(song)))
+        theory_dictionary["key"].append(get_key(song))
+        theory_dictionary["mode"].append(get_mode(song))
+        theory_dictionary["mood"].append(float(get_mood(song)))
+        theory_dictionary["danceability"].append(float(get_danceability(song)))
+        theory_dictionary["acousticness"].append(float(get_acousticness(song)))
+        theory_dictionary["energy"].append(float(get_energy(song)))
+        theory_dictionary["instrumentalness"].append(float(get_instrumentalness(song)))
+    # print(compute_top_songs_theory_helper({'tempo': [95.025, 106.973, 85.012, 77.332, 85.016, 195.09], 'time_signature': [4, 4, 4, 4, 4, 4], 'key': ['3', '2', '3', '0', '2', '1'], 'mode': ['1', '1', '1', '1', '1', '1'], 'mood': [0.462, 0.228, 0.337, 0.943, 0.383, 0.451], 'danceability': [0.598, 0.563, 0.588, 0.553, 0.702, 0.52], 'acousticness': [0.215, 0.872, 0.0678, 0.839, 0.587, 0.464], 'energy': [0.72, 0.283, 0.521, 0.545, 0.597, 0.448], 'instrumentalness': [2.95e-06, 0.000143, 0.149, 0.0, 3.62e-06, 5.79e-06]}))
+    return compute_top_songs_theory_helper(theory_dictionary)
 
-
+#print(compute_top_songs_theory([['Bad Friend', 'Rina Sawayama'], ['cellophane', 'FKA twigs'], ['Daddy Issues', 'The Neighbourhood'], ['Strawberry Blond', 'Mitski'], ['F2020', 'Avenue Beat'], ['Cancelled.', 'Kiana Ledé']]))
 #########################
 ##### REPLY METHODS #####
 #########################
@@ -224,7 +236,7 @@ def reply_top_genres(time_range, limit):
                 output += " (" + str(index + 1) + ") " + genre 
             return output + ". Happy listening!"
     except:
-        print("An exception occurred. Something went wrong. :( uh oh")
+        return "An exception occurred. Something went wrong. :( uh oh"
     
 def reply_top_songs(time_range, limit):
     """Returns a string with a user's top songs that the Discord Bot will reply to the chat 
@@ -254,7 +266,7 @@ def reply_top_songs(time_range, limit):
                 output += " (" + str(index + 1) + ") " + song_details[0] + " by " + song_details[1]
             return output + ". Nice bops!"
     except:
-        print("An exception occurred. Something went wrong. :( uh oh")
+        return "An exception occurred. Something went wrong. :( uh oh"
 
 def reply_top_artists(time_range, limit):
     """Returns a string with a user's top artists that the Discord Bot will reply to the chat 
@@ -283,7 +295,7 @@ def reply_top_artists(time_range, limit):
                 output += " (" + str(index + 1) + ") " + artist
             return output + ". You have great taste!"
     except:
-        print("An exception occurred. Something went wrong. :( uh oh")
+        return "An exception occurred. Something went wrong. :( uh oh"
 
 def reply_top_songs_theory(time_range, limit):
     """Returns a string with a user's theory data on their top songs over a given time range that
@@ -302,12 +314,49 @@ def reply_top_songs_theory(time_range, limit):
     :rtype: string
     :return: a string that describes what a user's theory data on top songs
     """
-    top_songs = list(compute_top_songs(time_range, limit).values())
-    print(compute_top_songs_theory(top_songs))
-    # Add try and except block
-    # calls compute_top_songs and passes the output to compute_top_songs_theory 
-    # stores the output of compute_top_songs_theory in a dictionary
-    # loop through the dictionary and format a string ("Your top songs have an average tempo of ...,  your most common key(s) is/are … "). Inside the loop, there should an if/else to check if the length of the length of the value is greater than 1 (there was a tie). If there a tie is found, the string should use the “are” word instead of “is.” The noun should also be plural instead of singular. 
-    # Return string 
+    output = "Your top song"
+    if limit < 0:
+        return "Please try a limit greater than 0! :)"
+    elif limit == 1:
+        output += "has the following music theory features:"
+    else:
+        output += "s have the following music theory features:"
 
-print(reply_top_songs_theory("medium_term", 6))
+    try:
+        top_songs = list(compute_top_songs(time_range, limit).values())
+        #theory_dictionary = {'tempo': 195.09, 'time_signature': 4, 'key': ['3', '2'], 'mode': ['1', '0'], 'mood': 0.451, 'danceability': 0.52, 'acousticness': 0.464, 'energy': 0.448, 'instrumentalness':  3.62e-06}
+        theory_dictionary = (compute_top_songs_theory(top_songs))
+        for feature in theory_dictionary:
+            if feature == "key":
+                key_dict = {"0": "C",
+                            "1": "C#/D♭",
+                            "2": "D",
+                            "3": "D#/E♭",
+                            "4": "E",
+                            "5": "F",
+                            "6": "F#/G♭",
+                            "7": "G",
+                            "8": "G#/A♭",
+                            "9": "A",
+                            "10": "A#/B♭",
+                            "11": "B"}
+                if len(theory_dictionary[feature]) == 1:
+                    output += "\n• modal key of " + key_dict[theory_dictionary[feature][0]] + "."
+                else: 
+                    top_keys = ""
+                    for key in theory_dictionary[feature]:
+                        top_keys += key_dict[key] + ", "
+                    output += "\n• modal keys of " + top_keys[:-2] + "."
+            elif feature == "mode":
+                mode_dict = {"1": "major", "0": "minor"}
+                if len(theory_dictionary[feature]) == 1:
+                    output += "\n• modal modality of " + mode_dict[(theory_dictionary[feature][0])] + "."
+                else: 
+                    output += "\n• modal modalities of major and minor."
+            else:
+                output += "\n• mean " + str(feature) + " of " + str(theory_dictionary[feature]) + "."
+        return output
+    except:
+        return "An exception occurred. Something went wrong. :( uh oh"
+
+
