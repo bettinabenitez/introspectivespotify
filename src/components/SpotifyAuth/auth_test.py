723cabe35f63ddf8cda9c3989ad0ae88aef79e31
William-La <wla@g.hmc.edu>
2020-11-29 22:27:31 -0800

    PAIR PROGRAMMING: Emily - write test_spotify_id test

--- src/components/SpotifyAuth/auth_test.py
+++ src/components/SpotifyAuth/auth_test.py
@@ -1,5 +1,4 @@
-import unittest
-import SpotifyAuth.SpotifyAuth
+import SpotifyAuth.SpotifyAuth as SpotifyAuth
 
 # BLACK BOX TESTS
 async def test_login(bot, user):
@@ -15,8 +14,11 @@ def test_refresh_token(user):
     pass
 
 def test_spotify_id(user):
-    pass
+    result = SpotifyAuth.get_spotify_id(user)
 
+    assert result == "tenzyn"
+
+    return "test_spotify_id pass\n"
 
 # WHITE BOX TESTS
 def test_login_permissions(user):
@@ -26,10 +28,15 @@ def test_discord_username_change(user):
     pass
 
 async def test_all_auth(bot, user):
-    await test_login(bot, user)
-    test_logout(user)
-    test_get_token(user)
-    test_refresh_token(user)
-    test_spotify_id(user)
-    test_login_permissions(user)
-    test_discord_username_change(user)
+    test_string = ""
+    # test_string += await test_login(bot, user)
+    # test_string += test_logout(user)
+    # test_string += test_get_token(user)
+    # test_string += test_refresh_token(user)
+    test_string += test_spotify_id(user)
+    # test_string += test_login_permissions(user)
+    # test_string += test_discord_username_change(user)
+    
+    test_results = open("test_results.txt", "w")
+    test_results.write(test_string)
+    test_results.close()
