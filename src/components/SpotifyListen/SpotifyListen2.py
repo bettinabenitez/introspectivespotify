cabc3956dcfdc839c795c93af7385be54217f459
emily <emilylu232@gmail.com>
2021-03-30 23:24:28 -0700

    wrote docstrings and fixed comments
    Co-authored-by: bettinabenitez <bettinabenitez@users.noreply.github.com>

--- src/components/SpotifyListen/SpotifyListen2.py
+++ src/components/SpotifyListen/SpotifyListen2.py
@@ -17,9 +17,6 @@ REDIRECT_URI = os.getenv("REDIRECT_URI")
 # current_song = ""
 # current_pos = 0
 
-
-
-
 info = []  # info[0] = spotify user, info[1] = playlist_id
 total_number_songs = 0
 listening_party = False
@@ -29,95 +26,42 @@ queue = []
 scope = "user-read-recently-played, user-top-read, user-read-playback-position, user-read-playback-state, user-modify-playback-state, user-read-currently-playing, playlist-modify-public, user-read-private, playlist-modify-private, playlist-read-private, playlist-read-collaborative"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=scope, cache_path=".oAuthCache"))
 
-##### new design idea - make playlist for each listening party #####
-
-## methods that need to change
-# ALMOST DONE?: addSong --- needs to add song to playlist (not to user's queue)
-# remove song --- playlist_remove_all_occurrences_of_items or playlist_remove_specific_occurrences_of_items
-# displayQueue --- call getQueue and format 
-# getQueue ---
-    # call playlist_tracks from Spotipy and loop through the output to get ID, name, and artist(s) 
-    # look at recently played
-# shuffle ---
-    # can we change all of the songs in playlist? yes we can!
-    # Or we can take off all the items after our current song and readd them in a diff order
-
-## methods that can stay the same
-# getCurrentSong 
-# skip 
-# play/pause 
-
-## lingering questions
-# do we still need to keep track of the queue in a list? or can we just let spotify handle it?
-    # answer: maybe don't need to keep track. we can get the queue by getting current song + comparing to tracks in playlist
-    # depends on if we want our queue to automatically store name, artist, ID
-
 # TODO: figure out why strings dont fucking work!!!
 # TODO: Assign people to roles/groups -> those roles and those people in the listening party this is instead of using the database DEVIKA WILL HELP
 # TODO: Prepare for merge    
-
+# TODO: what if you call any of the methods when listening party has not started yet? :(
 ## another idea: get everyone's spotify ID and play the playlist for everyone. SO pause can finally work :)
 
 def getCurrentSong():
-    """
-    returns the current song the listening party is listening to 
+    """ gets information about listening party's current song 
 
-    Accessor method that gets the current song playing by the listening party. 
-    Call setCurrentSong() to get the most updated song that the users are listening to
-
-
-    Returns:
-        string: Name of the current song being played in the listening party
-
-        TODO: fix comments
-
+    :rtype: tuple
+    :returns: current song (string) and current position (int)
     """
     current_song = ""
     current_pos = 0
     
-    # get info about user's current song 
+    # get user's current song's ID
     results = sp.current_user_playing_track()
-
     trackID = results['item']['id']
 
-    if trackID != "": # check if there is a song playing
+    # check if there is a song playing and set global variables
+    if trackID != "": 
         current_song = trackID
         current_pos = results['progress_ms']
 
-    # print(f"Current position: {current_pos}")
     return (current_song, current_pos)
-    
-        
-def getQueue(spotify_id):
-    """
-    get queue that we have stored (not spotify user's queue)
-    returns a list
-
-    Acessor method that gets songs added in the listening party’s queue with the current song as the first item of the list. 
-    This is done by selecting the songs from the list on the song database that are currently on the list of being played.
-
-    
-    Returns:
-        list: track songs of songs currently on the listening party’s queue
-
-
-    TODO: fix comments is this needed?
-    """
-    return queue
 
 def display_queue():
-    """
-    show users what the queue is 
-    returns a string 
+    """ Displays songs added in the listening party’s playlist with
+    the current song as the first item 
 
-    Displays songs added in the listening party’s queue with the current song as the first item of the list. 
-    This is done by calling the getQueue function then using discord API to send a message on the discord channel.
-
-    TODO: just do this lmao
+    :rtype: string 
+    :returns a formatted string describing the queue 
     """
     # pass in playlist_id
     queue = sp.playlist_items(playlist_id=info[1])
-    print(queue)
+
 
     output = "Your songs in the queue are: \n \n" 
 
@@ -136,104 +80,79 @@ def display_queue():
         output += str(counter) + ". " + trackName + " by " + trackArtist + "\n"
         counter += 1
 
-        # TO DO: 
-
+    # TODO: finish this method (only display songs in the queue, not whole playlist)
+    # helpful link?: https://github.com/spotify/web-api/issues/1288
+    # "you can combine the Get Current Playback with Get Recently Played which will
+    # give you the previous 50 tracks played (provided the user played at least 30
+    # seconds of the track). If you combine that with Get Playlists Tracks, you
+    # should be able to piece together what the active index is.
+    
     return output
 
 
 def play_party():
-    """
-    adds a song to the user's Spotify queue and also to our queue 
-    void
+    """ plays Spotify user's Spotify 
 
-    Plays the playback for all users in the listening party.
-    This is done by using the spotipy object to play the song on the user's playback.
-    Note: If the song on the user is currently playing, this function will not run. 
+    :rtype: string
+    :returns a notification to user  
 
-    TODO: fix comments
-          add play song?
+    TODO: add play song? what happens if you say play before start listening party? 
     """
     sp.start_playback()
 
-    return " Listening Party is playing"
+    return "Listening Party is playing"
+
 
 def pause_party():
+    """ pauses Spotify user's Spotify 
+
+    :rtype: string
+    :returns a notification to user 
+
+    TODO: currently only pauses for host. Need to fix by merging w William's branch
     """
-    pauses Spotify user's Spotify 
-    void 
-
-    Pauses the playback for all users in the listening party.
-    This is done by using the spotipy object to pause the song on the user's playback.
-    Note: If the song on the user is currently paused, this function will not run.
-
-    TODO: fix comments
-
-    """
-
     sp.pause_playback()
 
     return "Listening Party is paused (only for host)"
 
+
 def skip_party(user):
+    """ Skips the current song 
+    
+    :rtype: string
+    :returns a notification to user 
     """
-    skips the current song 
-    void
-
-    Skips the playback for all users in the listening party.
-    This is done by selecting the song on the queue from the database that is next on the queue being played
-    then using the spotify object to play that song for all the users
-
-    """
-
     sp.next_track()
 
     return user + " skipped to the next track"
 
+
 def rewind_party(user):
-    """
-    rewind to previous song on the queue
-    void
-
-    Rewinds the playback for all users in the listening party.
-    This is done by selecting the song on the queue from the database prior to song currently being played 
-    then using the spotipy object to play that song for all the users
-
-    TODO: fix comments
-
+    """ Rewinds to beginning of song OR previous song on the queue
+    
+    rtype: string
+    returns a notification to user 
     """
     current_song = getCurrentSong()
 
-    # we have played over 5 seconds of the current song
-    # rewind to beginning
+    # rewind to beginning if we have played over 5 seconds of the current song
     if current_song[1] >= 5000:  # 5 seconds
         sp.seek_track(0)
         return user + " rewinded song to beginning"
     
+    # rewind to previous song 
     sp.previous_track()
     return user + " rewinded song to previous song"
 
+
 def add_song(song):
-    """
-    add it user's queue and to our own queue 
-    void 
-
-    Adds a song to the playback for all users in the listening party.
-    This is done by using the Spotipy object to search for the song.
-    Parameters needed:
-        Q: q=song%20 (any spaces must be separated by %20)
-        Type: type=track
-        Limit: limit=1 
-    The response of the search is in JSON, and will return the songs that correspond with the song searched
-    Therefore this method will parse through the JSON and retrieve the track id
-    Increment total_number_songs variable by 1
-    Then add the trackid, song name, and position in the queue (total number of songs), and status (2) to the database.
-    The song variable will also be saved under the discord direct quote column.
-
-    Args:
-        song (string): name of the song
-
-        TODO: fix comments
-
+    """ add a song user's Spotify queue and to our own queue 
+    :param song: name of the song
+    :type song: string
+    
+    :rtype: string
+    :returns a notification to user letting them know a song was added 
+    or that no active device was found
     """
     results = sp.search(q= song, type="track", limit=1)
     
@@ -253,10 +172,9 @@ def add_song(song):
     # find device to play on
     id_found = ""
     devices = sp.devices()
-    #print(f"devices are here: {sp.devices()}")
     for device in devices['devices']:
-        #print(device)
-        #print(".....")
+
+        # active device was found, so get device ID 
         if device['is_active'] == True:
             id_found = device['id']
             sp.shuffle(state = False, device_id=id_found)
@@ -276,22 +194,15 @@ def add_song(song):
 
     return trackName + " by " + trackArtist + " was added to the queue"
 
-def remove(self, song, listening_party_id):
+
+def remove(song):
+    """ removes a song from user's Spotify queue (the playlist)
+    
+    :param song: name of the song
+    :type song: string
+    
+    # questions: remove all occurences or just one?  do we remove from our queue too? 
     """
-    remove a song from our queue (just the first time we see it)
-
-    removes a song to the playback for all users in the listening party.
-    This is done by selecting the song from the database either from direct quote or song name and deleting it from the database. 
-    Decrement all the positions of the songs after the deleted song by 1
-    Decrement the total_number_songs variable by 1
-    Note: By removing a song the song will not be added if a playlist is created
-    If there are no songs on in the queue at all, then nothing is run
-
-    Args:
-        song (string): name of the song
-
-    """
-
     results = sp.search(q= song, type="track", limit=1)
 
     # Iterate through the results specifically for tracks in items and grab
@@ -302,17 +213,49 @@ def remove(self, song, listening_party_id):
     if trackID not in q:
         return song + "not in queue"
     
+    # This the
     q = q.remove(trackID)
     return song + "was removed from queue"
 
+    # TODO: write this method with new design
+    # playlist_remove_specific_occurrences_of_items(playlist_id, items, snapshot_id=None)
+        # Removes all occurrences of the given tracks from the given playlist
+        # Parameters:
+            # playlist_id - the id of the playlist
+            # items - an array of objects containing Spotify URIs of the
+
+
+def shuffle():
+    """ Shuffles the listening party's playlist 
+
+    :rtype: string
+    :returns a notification to user saying we shuffled
+    """
+    # TODO: write this method 
+    # reorder songs in playlist
+    # Parameters:
+        # playlist_id - the id of the playlist
+        # range_start - the position of the first track to be reordered
+        # range_length - optional the number of tracks to be reordered
+        # (default: 1)
+        # insert_before - the position where the tracks should be
+
+
 def getUserID():
+    """
+    :rtype: string
+    :returns user's spotify ID
+    """
     return sp.current_user()['id']
 
+
 def start_listening_party(playlist_name):
-    """
-    start the listening party
-    create a playlist + start playing the playlist?
-    
+    """ Starts the listening party
+    :param playlist_name: name of playlist
+    :type song: string
+
+    :rtype: string
+    :returns a notification to user saying we started the listening party 
     """
     # store user ID
     info.append(getUserID())
@@ -327,30 +270,19 @@ def start_listening_party(playlist_name):
 
 
 def delete_playlist():
-    """
+    """ Deletes the listening party's playlist
+
+    :rtype: string
+    :returns a notification to user saying we deleted the playlist
     """   
     # pass in spotify user and playlist id 
     sp.user_playlist_unfollow(info[0], info[1])
     
-    return "we deleted the playlist for the listening party"
+    return "We deleted the playlist for the listening party!"
 
 
 ###############################################################
 # functions to implement later below!
-
-# def shuffle(self, user):
-#     """
-#     Shuffles the playback for all users in the listening party.
-#     This is done on the database by selecting all the songs that still need to be played and shuffling the order that it is to be played
-#     Use the Fisher-Yates algorithm to shuffle songs on the playlist, which uses the random function from the random library to randomize the selection of songs.
-#     Call queueManagement() method to ensure that the queue is correct
-#     Get the position number of the first song after the current song
-#     Create a random list of position numbers starting from the position retrieved and the number of songs (use rand() function from random library).
-#     Call queueManagement() method to rearrange the queue to the new shuffled playlist.
-#     If there are no songs on in the queue at all, then nothing is run
-
-#     """
-
 
 # def join(self, user):
 #     """
@@ -395,3 +327,20 @@ def delete_playlist():
 #         return user.name + " has left the listening party and the listening party is over. :("
 
 #     return user.name + " has left the listening party."
+
+
+# def getQueue():
+#     """ get queue based on playlist
+#     returns a list
+
+#     Acessor method that gets songs added in the listening party’s queue with the current song as the first item of the list. 
+#     This is done by selecting the songs from the list on the song database that are currently on the list of being played.
+
+    
+#     Returns:
+#         list: track songs of songs currently on the listening party’s queue
+
+
+#     TODO: fix comments is this needed?
+#     """
+#     return queue
